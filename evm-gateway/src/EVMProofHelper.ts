e09bf3e29ce45f436b64df07ae2b045988905aa4
Nick Johnson <arachnid@notdot.net>
2023-10-05 12:38:36 +0100

    Apply prettier suggestions

--- evm-gateway/src/EVMProofHelper.ts
+++ evm-gateway/src/EVMProofHelper.ts
@@ -4,21 +4,21 @@ import { ethers } from 'ethers';
  * Response of the eth_getProof RPC method.
  */
 interface EthGetProofResponse {
-    accountProof: string[];
-    balance: string;
-    codeHash: string;
-    nonce: string;
-    storageHash: string;
-    storageProof: {
-        key: string;
-        value: string;
-        proof: string[];
-    }[];
+  accountProof: string[];
+  balance: string;
+  codeHash: string;
+  nonce: string;
+  storageHash: string;
+  storageProof: {
+    key: string;
+    value: string;
+    proof: string[];
+  }[];
 }
 
 export interface StateProof {
-    stateTrieWitness: string[];
-    storageProofs: string[][];
+  stateTrieWitness: string[];
+  storageProofs: string[][];
 }
 
 /**
@@ -27,40 +27,52 @@ export interface StateProof {
  *
  */
 export class EVMProofHelper {
-    private readonly provider: ethers.JsonRpcProvider;
+  private readonly provider: ethers.JsonRpcProvider;
 
-    constructor(provider: ethers.JsonRpcProvider) {
-        this.provider = provider;
-    }
+  constructor(provider: ethers.JsonRpcProvider) {
+    this.provider = provider;
+  }
 
-    /**
-     * @dev Returns the value of a contract state slot at the specified block
-     * @param block A `ProvableBlock` returned by `getProvableBlock`.
-     * @param address The address of the contract to fetch data from.
-     * @param slot The slot to fetch.
-     * @returns The value in `slot` of `address` at block `block`
-     */
-    getStorageAt(blockNo: number, address: ethers.AddressLike, slot: bigint): Promise<string> {
-        return this.provider.getStorage(address, slot, blockNo);
-    }
+  /**
+   * @dev Returns the value of a contract state slot at the specified block
+   * @param block A `ProvableBlock` returned by `getProvableBlock`.
+   * @param address The address of the contract to fetch data from.
+   * @param slot The slot to fetch.
+   * @returns The value in `slot` of `address` at block `block`
+   */
+  getStorageAt(
+    blockNo: number,
+    address: ethers.AddressLike,
+    slot: bigint
+  ): Promise<string> {
+    return this.provider.getStorage(address, slot, blockNo);
+  }
 
-    /**
-     * @dev Fetches a set of proofs for the requested state slots.
-     * @param block A `ProvableBlock` returned by `getProvableBlock`.
-     * @param address The address of the contract to fetch data from.
-     * @param slots An array of slots to fetch data for.
-     * @returns A proof of the given slots, encoded in a manner that this service's
-     *   corresponding decoding library will understand.
-     */
-    async getProofs(blockNo: number, address: ethers.AddressLike, slots: bigint[]): Promise<StateProof> {
-        const args = [address, slots.map((slot) => ethers.toBeHex(slot, 32)), ethers.toBeHex(blockNo)];
-        const proofs: EthGetProofResponse = await this.provider.send(
-            'eth_getProof',
-            args
-        );
-        return {
-            stateTrieWitness: proofs.accountProof,
-            storageProofs: proofs.storageProof.map((proof) => proof.proof),
-        };
-    }
+  /**
+   * @dev Fetches a set of proofs for the requested state slots.
+   * @param block A `ProvableBlock` returned by `getProvableBlock`.
+   * @param address The address of the contract to fetch data from.
+   * @param slots An array of slots to fetch data for.
+   * @returns A proof of the given slots, encoded in a manner that this service's
+   *   corresponding decoding library will understand.
+   */
+  async getProofs(
+    blockNo: number,
+    address: ethers.AddressLike,
+    slots: bigint[]
+  ): Promise<StateProof> {
+    const args = [
+      address,
+      slots.map(slot => ethers.toBeHex(slot, 32)),
+      ethers.toBeHex(blockNo),
+    ];
+    const proofs: EthGetProofResponse = await this.provider.send(
+      'eth_getProof',
+      args
+    );
+    return {
+      stateTrieWitness: proofs.accountProof,
+      storageProofs: proofs.storageProof.map(proof => proof.proof),
+    };
+  }
 }
