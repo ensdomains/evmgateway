658d0119d57520f7d6aa9f54dc99a56194217149
Nick Johnson <arachnid@notdot.net>
2023-10-05 12:08:51 +0100

    Package tidyups, write test script for l1-verifier.

--- l1-verifier/hardhat.config.ts
+++ l1-verifier/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    ganache: {
+      url: `http://localhost:${parseInt(process.env['RPC_PORT'] || '8545')}`
+    }
+  }
 };
 
 export default config;
