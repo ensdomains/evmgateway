4d58ed246311239a653eae6e271846e50c9fc4ed
tate <tate@ens.domains>
2023-10-09 10:50:36 +1100

    gha

--- scripts/workspace.ts
+++ scripts/workspace.ts
@@ -1,4 +1,4 @@
-import { join } from "node:path";
+import { join } from 'node:path';
 
 const workspace = process.argv[2];
 const script = process.argv[3];
@@ -6,20 +6,22 @@ const script = process.argv[3];
 const runScriptForWorkspace = (w: string) => {
   console.log(`Running script "${script}" in workspace "${w}"`);
   Bun.spawn({
-    cmd: ["bun", "run", script],
+    cmd: ['bun', 'run', script],
     cwd: `./${w}`,
     stdio: ['inherit', 'inherit', 'inherit'],
   });
-}
+};
 
-
-if (workspace !== "all")
+if (workspace !== 'all' && !workspace.startsWith('-'))
   runScriptForWorkspace(workspace);
 else {
-  const packageJson = await Bun.file(join(import.meta.dir, "../package.json")).json();
+  const excluded = workspace.startsWith('-') && workspace.slice(1);
+  const packageJson = await Bun.file(
+    join(import.meta.dir, '../package.json')
+  ).json();
   const workspaces = packageJson.workspaces.packages as string[];
   for (const w of workspaces) {
+    if (w === excluded) continue;
     runScriptForWorkspace(w);
   }
 }
-
