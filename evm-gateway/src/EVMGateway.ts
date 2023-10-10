4969d9622285dd949ad3d7c4b32cbb8eaa88e4cb
Nick Johnson <arachnid@notdot.net>
2023-10-10 12:47:57 +0100

    Add comments describing the VM; run lint.

--- evm-gateway/src/EVMGateway.ts
+++ evm-gateway/src/EVMGateway.ts
@@ -48,6 +48,40 @@ export class EVMGateway<T extends ProvableBlock> {
   makeApp(path: string) {
     const server = new Server();
     const abi = [
+      /**
+       * This function implements a simple VM for fetching proofs for EVM contract storage data.
+       * Programs consist of an array of `commands` and an array of `constants`. Each `command` is a
+       * short program that computes the slot number of a single EVM storage value. The gateway then
+       * returns a proof of a value at that slot number. Commands can also specify that the value is
+       * dynamic-length, in which case the gateway may return proofs for multiple slots in order for
+       * the caller to be able to reconstruct the entire value.
+       *
+       * Each command is a 32 byte value consisting of a single flags byte, followed by 31 instruction
+       * bytes. Valid flags are:
+       *  - 0x01 - If set, the value to be returned is dynamic length.
+       *
+       * The VM implements a very simple stack machine, and instructions specify operations that happen on
+       * the stack. In addition, the VM has access to the result of previous commands, referred to here
+       * as `values`.
+       *
+       * The most significant 3 bits of each instruction byte are the opcode, and the least significant
+       * 5 bits are the operand. The following opcodes are defined:
+       *  - 0x00 - `push(constants[operand])`
+       *  - 0x20 - `push(values[operand])`
+       *  - 0x70 - `halt` - do not process any further instructions for this command.
+       *
+       * After a `halt` is reached or the end of the command word is reached, the elements on the stack
+       * are hashed recursively, starting with the first element pushed, using a process equivalent
+       * to the following:
+       *   def hashStack(stack):
+       *     right = stack.pop()
+       *     if(stack.empty()):
+       *       return right
+       *     return keccak256(concat(hashStack(stack), right))
+       *
+       * The final result of this hashing operation is used as the base slot number for the storage
+       * lookup. This mirrors Solidity's recursive hashing operation for determining storage slot locations.
+       */
       'function getStorageSlots(address addr, bytes32[] memory commands, bytes[] memory constants) external view returns(bytes memory witness)',
     ];
     server.add(abi, [
@@ -69,13 +103,23 @@ export class EVMGateway<T extends ProvableBlock> {
    * @param paths Each element of this array specifies a Solidity-style path derivation for a storage slot ID.
    *              See README.md for details of the encoding.
    */
-  async createProofs(address: string, commands: string[], constants: string[]): Promise<string> {
+  async createProofs(
+    address: string,
+    commands: string[],
+    constants: string[]
+  ): Promise<string> {
     const block = await this.proofService.getProvableBlock();
     const requests: Promise<StorageElement>[] = [];
     // For each request, spawn a promise to compute the set of slots required
     for (let i = 0; i < commands.length; i++) {
       requests.push(
-        this.getValueFromPath(block, address, commands[i], constants, requests.slice())
+        this.getValueFromPath(
+          block,
+          address,
+          commands[i],
+          constants,
+          requests.slice()
+        )
       );
     }
     // Resolve all the outstanding requests
@@ -86,18 +130,21 @@ export class EVMGateway<T extends ProvableBlock> {
     return this.proofService.getProofs(block, address, slots);
   }
 
-  private async executeOperation(operation: number, constants: string[], requests: Promise<StorageElement>[]): Promise<string> {
+  private async executeOperation(
+    operation: number,
+    constants: string[],
+    requests: Promise<StorageElement>[]
+  ): Promise<string> {
     const opcode = operation & 0xe0;
     const operand = operation & 0x1f;
 
-    switch(opcode) {
-    case OP_CONSTANT:
-      return constants[operand];
-    case OP_BACKREF:
-      const targetElement = await requests[operand];
-      return await targetElement.value();
-    default:
-      throw new Error("Unrecognized opcode");
+    switch (opcode) {
+      case OP_CONSTANT:
+        return constants[operand];
+      case OP_BACKREF:
+        return await (await requests[operand]).value();
+      default:
+        throw new Error('Unrecognized opcode');
     }
   }
 
@@ -110,11 +157,17 @@ export class EVMGateway<T extends ProvableBlock> {
     const flags = commandWord[0];
     const isDynamic = (flags & 0x01) != 0;
 
-    let slot = toBigInt(await this.executeOperation(commandWord[1], constants, requests));
+    let slot = toBigInt(
+      await this.executeOperation(commandWord[1], constants, requests)
+    );
 
     // If there are multiple path elements, recursively hash them solidity-style to get the final slot.
     for (let j = 2; j < 32 && commandWord[j] != 0xff; j++) {
-      let index = await this.executeOperation(commandWord[j], constants, requests);
+      const index = await this.executeOperation(
+        commandWord[j],
+        constants,
+        requests
+      );
       slot = toBigInt(
         solidityPackedKeccak256(['bytes', 'uint256'], [index, slot])
       );
@@ -169,7 +222,11 @@ export class EVMGateway<T extends ProvableBlock> {
     constants: string[],
     requests: Promise<StorageElement>[]
   ): Promise<StorageElement> {
-    const { slot, isDynamic } = await this.computeFirstSlot(command, constants, requests);
+    const { slot, isDynamic } = await this.computeFirstSlot(
+      command,
+      constants,
+      requests
+    );
 
     if (!isDynamic) {
       return {
