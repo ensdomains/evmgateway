4daafd6bbff6940ba0e6a767529f68556daf0d9a
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-13 15:48:56 +0100

    Move PROVIDER_URL to secret

--- l1-gateway-worker/src/server.ts
+++ l1-gateway-worker/src/server.ts
@@ -1,10 +1,18 @@
-import { EVMGateway } from '@ensdomains/evm-gateway';
-import { ethers } from 'ethers';
-import { L1ProofService } from './L1ProofService.js';
+// import { EVMGateway } from '@ensdomains/evm-gateway';
+// import { ethers } from 'ethers';
+// import { L1ProofService } from './L1ProofService.js';
+// import { EVMGateway } from '@ensdomains/l1-gateway';
 import { Server } from '@ensdomains/ccip-read-cf-worker';
 
-function fetch(request:any, env:any, _context:any){
-  console.log({request}, {env},{ _context})
+async function fetch(request:any, env:any, _context:any){
+  // Loading libraries dynamically as a temp work around.
+  // Otherwise, deployment thorws "Error: Script startup exceeded CPU time limit." error
+  const ethers = await import('ethers');
+  const { EVMGateway } = await import('@ensdomains/evm-gateway');
+  const { L1ProofService } = await import('./L1ProofService');
+  // import { Server } from '@ensdomains/ccip-read-cf-worker';
+
+  // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
   const { PROVIDER_URL } = env;
   console.log({PROVIDER_URL})
   const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
