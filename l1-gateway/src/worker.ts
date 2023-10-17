0eeb4b4deffb9f1d340a51d7b26dd83f66d26b52
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-17 16:04:38 +0100

    Cache app

--- l1-gateway/src/worker.ts
+++ l1-gateway/src/worker.ts
@@ -1,23 +1,26 @@
 import { Server } from '@ensdomains/ccip-read-cf-worker';
+
 interface Env {
   WORKER_PROVIDER_URL: string
 }
-let ethers:any, EVMGateway:any, L1ProofService:any
+interface Router{
+  handle:(request:Request)=> void
+}
+let app:Router
 async function fetch(request:Request, env:Env){
   // Loading libraries dynamically as a temp work around.
   // Otherwise, deployment thorws "Error: Script startup exceeded CPU time limit." error
-  if(!ethers) ethers = await import('ethers');
-  if(!EVMGateway) EVMGateway = (await import('@ensdomains/evm-gateway')).EVMGateway
-  if(!L1ProofService) L1ProofService = (await import('./L1ProofService.js')).L1ProofService;
-  // const { EVMGateway } = await import('@ensdomains/evm-gateway');
-  // const { L1ProofService } = await import('./L1ProofService.js');
-  // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
-  const { WORKER_PROVIDER_URL } = env;
-  const provider = new ethers.JsonRpcProvider(WORKER_PROVIDER_URL);
-  const proof = new EVMGateway(new L1ProofService(provider));
-  const server = new Server();
-  proof.add(server);
-  const app = server.makeApp("/")
+  if(!app){
+    const ethers = await import('ethers');
+    const EVMGateway = (await import('@ensdomains/evm-gateway')).EVMGateway
+    const L1ProofService = (await import('./L1ProofService.js')).L1ProofService;
+    const { WORKER_PROVIDER_URL } = env;
+    const provider = new ethers.JsonRpcProvider(WORKER_PROVIDER_URL);
+    const proof = new EVMGateway(new L1ProofService(provider));
+    const server = new Server();
+    proof.add(server);
+    app = server.makeApp("/")  
+  }
   return app.handle(request)
 }
 
--- op-gateway/src/worker.ts
+++ op-gateway/src/worker.ts
@@ -4,25 +4,31 @@ interface Env {
     L2_PROVIDER_URL: string
     DELAY: number
 }
+interface Router{
+  handle:(request:Request)=> void
+}
+let app:Router  
 async function fetch(request:Request, env:Env){
   // Loading libraries dynamically as a temp work around.
   // Otherwise, deployment thorws "Error: Script startup exceeded CPU time limit." error
-  const { EVMGateway } = await import('@ensdomains/evm-gateway');
-  const { OPProofService } = await import('./OPProofService.js');
-  // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
-  const { L1_PROVIDER_URL, L2_PROVIDER_URL, DELAY } = env;
-  console.log(env)
-  const proof = new EVMGateway(
-    await OPProofService.create(
-      L1_PROVIDER_URL,
-      L2_PROVIDER_URL,
-      Number(DELAY)
-    )
-  );
-
-  const server = new Server();
-  proof.add(server);
-  const app = server.makeApp("/")
+  if(!app){
+    const EVMGateway = (await import('@ensdomains/evm-gateway')).EVMGateway
+    const OPProofService = (await import('./OPProofService.js')).OPProofService;
+  
+    // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
+    const { L1_PROVIDER_URL, L2_PROVIDER_URL, DELAY } = env;
+    const proof = new EVMGateway(
+      await OPProofService.create(
+        L1_PROVIDER_URL,
+        L2_PROVIDER_URL,
+        Number(DELAY)
+      )
+    );
+  
+    const server = new Server();
+    proof.add(server);
+    app = server.makeApp("/")  
+  }
   return app.handle(request)
 }
 
