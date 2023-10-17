8c7379f426637dd9ed77610c019e6dce7fccfa9f
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-17 12:37:07 +0100

    Cache dynamic library loading

--- l1-gateway/src/worker.ts
+++ l1-gateway/src/worker.ts
@@ -2,15 +2,17 @@ import { Server } from '@ensdomains/ccip-read-cf-worker';
 interface Env {
   WORKER_PROVIDER_URL: string
 }
+let ethers:any, EVMGateway:any, L1ProofService:any
 async function fetch(request:Request, env:Env){
   // Loading libraries dynamically as a temp work around.
   // Otherwise, deployment thorws "Error: Script startup exceeded CPU time limit." error
-  const ethers = await import('ethers');
-  const { EVMGateway } = await import('@ensdomains/evm-gateway');
-  const { L1ProofService } = await import('./L1ProofService.js');
+  if(!ethers) ethers = await import('ethers');
+  if(!EVMGateway) EVMGateway = (await import('@ensdomains/evm-gateway')).EVMGateway
+  if(!L1ProofService) L1ProofService = (await import('./L1ProofService.js')).L1ProofService;
+  // const { EVMGateway } = await import('@ensdomains/evm-gateway');
+  // const { L1ProofService } = await import('./L1ProofService.js');
   // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
   const { WORKER_PROVIDER_URL } = env;
-  console.log({WORKER_PROVIDER_URL})
   const provider = new ethers.JsonRpcProvider(WORKER_PROVIDER_URL);
   const proof = new EVMGateway(new L1ProofService(provider));
   const server = new Server();
