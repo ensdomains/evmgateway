005f988ce3636f85bde40f66e887bed37c4db3d5
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-20 13:39:47 +0100

    Fix hardhat config error

--- op-verifier/hardhat.config.ts
+++ op-verifier/hardhat.config.ts
@@ -3,8 +3,8 @@ import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
 import { HardhatUserConfig } from 'hardhat/config';
 import 'ethers'
-const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY ?? "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const L1_PROVIDER_URL = process.env.L1_PROVIDER_URL
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const L1_PROVIDER_URL = process.env.L1_PROVIDER_URL || '';
 const L1_ETHERSCAN_API_KEY = process.env.L1_ETHERSCAN_API_KEY;
 const L2_ETHERSCAN_API_KEY = process.env.L2_ETHERSCAN_API_KEY;
 
