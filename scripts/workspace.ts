d2237126ca0193d57f428c707c44c9e30846a803
tate <tate@ens.domains>
2023-10-23 10:27:56 +1100

    hardhat

--- scripts/workspace.ts
+++ scripts/workspace.ts
@@ -1,24 +1,27 @@
+import { file } from 'bun';
 import { join } from 'node:path';
 
 const workspace = process.argv[2];
 const script = process.argv[3];
 
-const runScriptForWorkspace = (w: string) => {
-  console.log(`Running script "${script}" in workspace "${w}"`);
-  Bun.spawn({
-    cmd: ['bun', 'run', script],
-    cwd: `./${w}`,
-    stdio: ['inherit', 'inherit', 'inherit'],
-    onExit(exitCode:any) {
-      if(exitCode.exitCode !== 0) {
-        console.log('The script failed', exitCode)
-        throw('The script failed')
-      }else{
-        console.log('The script ran successfully')
-      }
-    }
+const runScriptForWorkspace = (w: string) =>
+  new Promise<number>((resolve, reject) => {
+    console.log(`Running script "${script}" in workspace "${w}"`);
+    Bun.spawn({
+      cmd: ['bun', 'run', script],
+      cwd: `./${w}`,
+      stdio: ['inherit', 'inherit', 'inherit'],
+      onExit(info) {
+        if (info.exitCode !== 0) {
+          console.log('The script failed', info.exitCode);
+          reject(info.exitCode);
+        } else {
+          console.log('The script ran successfully');
+          resolve(info.exitCode);
+        }
+      },
+    });
   });
-};
 
 if (workspace.includes(',')) {
   const workspaces = workspace.split(',');
@@ -29,12 +32,12 @@ if (workspace.includes(',')) {
   runScriptForWorkspace(workspace);
 else {
   const excluded = workspace.startsWith('-') && workspace.slice(1);
-  const packageJson = await Bun.file(
+  const packageJson = await file(
     join(import.meta.dir, '../package.json')
   ).json();
-  const workspaces = packageJson.workspaces.packages as string[];
+  const workspaces = packageJson.workspaces as string[];
   for (const w of workspaces) {
     if (w === excluded) continue;
-    runScriptForWorkspace(w);
+    await runScriptForWorkspace(w);
   }
 }
