79487ce2c41857d25562335384fcea4cd47db904
tate <tate@ens.domains>
2023-10-23 10:37:17 +1100

    fixed?

---
+++ l1-verifier/scripts/test.ts
@@ -0,0 +1,44 @@
+import ganache from 'ganache';
+import { spawn } from 'node:child_process';
+
+const options = {
+  logging: {
+    quiet: true,
+  },
+};
+
+async function main() {
+  const server = ganache.server(options);
+  console.log('Starting server');
+  await new Promise((resolve, reject) => {
+    server.listen(8888, async (err) => {
+      console.log(`Listening on port ${server.address().port}`);
+      if (err) reject(err);
+      resolve(server.address().port);
+    });
+  });
+
+  console.log('Starting hardhat');
+  await new Promise((resolve, reject) => {
+    const hh = spawn('hardhat', ['test', '--network', 'ganache'], {
+      stdio: 'inherit',
+    });
+    hh.on('close', (code) => {
+      console.log('Shutting down');
+      server.close();
+      if (code === 0) {
+        resolve(code);
+        process.exit(code);
+      } else {
+        reject(code);
+      }
+    });
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error('***err', error);
+  process.exit(error);
+});
