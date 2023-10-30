1de9d6f9567673d57e1da57bc63eed7a3bc73737
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-30 13:04:38 +0000

    Crosschain resolver (#7)
    * Make testResolver work
    
    * Update bun.lockb
    
    * Add coinType
    
    * Make text record work
    
    * Add hardhat-storage-layout
    
    * Use the real Resolver contracts
    
    * Add contenthash
    
    * Add comment
    
    * Update README
    
    * Change the license to ENS labs
    
    * Remove console.log
    
    * Add an explanation
    
    * typo
    
    * Remove L1Verifier.sol
    
    * Remove L1Verifier.sol
    
    * Rename test
    
    * Add hardhat-deploy
    
    * Add deployment script
    
    * Remove deployment addresses
    
    * Apply suggestions from code review
    
    Add indentation
    
    Co-authored-by: Nick Johnson <arachnid@notdot.net>
    
    * Add immutable and constants
    
    * Remove getStatic
    
    * Fix the wrong constants
    
    * Update bun.lockb
    
    * Remove contracts-bedrock
    
    * Update bun.lockb
    
    * Add bun
    
    * Remove package-lock.json
    
    * Revert test sript
    
    * update bun.lockb
    
    * Add ..
    
    * Use BrowserProvider
    
    * Revert to use fork
    
    * Put immutable and constant together
    
    * Use bytes20 instead
    
    * Apply suggestions from code review
    
    Co-authored-by: Nick Johnson <arachnid@notdot.net>
    
    * Remove gasLimit and gas
    
    * Add test for the empty record
    
    * Remove remote
    
    * Remove RECORD_VERSIONS_REF constant
    
    ---------
    
    Co-authored-by: Nick Johnson <arachnid@notdot.net>
    Co-authored-by: tate <tate@ens.domains>

---
+++ crosschain-resolver/contracts/deps.sol
@@ -0,0 +1,19 @@
+import {L1Verifier} from '@ensdomains/l1-verifier/contracts/L1Verifier.sol';
+import '@ensdomains/ens-contracts/contracts/resolvers/OwnedResolver.sol';
+
+// Storage slot
+// ┌─────────────────────┬──────────────────────────────┬──────────────┬
+// │      contract       │        state_variable        │ storage_slot │
+// ├─────────────────────┼──────────────────────────────┼──────────────┼
+// |    OwnedResolver    │            _owner            │      0       │
+// │    OwnedResolver    │        recordVersions        │      1       │
+// │    OwnedResolver    │       versionable_abis       │      2       │
+// │    OwnedResolver    │    versionable_addresses     │      3       │
+// │    OwnedResolver    │      versionable_hashes      │      4       │
+// │    OwnedResolver    │    versionable_zonehashes    │      5       │
+// │    OwnedResolver    │     versionable_records      │      6       │
+// │    OwnedResolver    │ versionable_nameEntriesCount │      7       │
+// │    OwnedResolver    │    versionable_interfaces    │      8       │
+// │    OwnedResolver    │      versionable_names       │      9       │
+// │    OwnedResolver    │     versionable_pubkeys      │      10      │
+// │    OwnedResolver    │      versionable_texts       │      11      │
