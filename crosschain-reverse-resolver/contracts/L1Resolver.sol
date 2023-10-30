74bb4ccdf9f2dec4e606bf602fc7c047ff9c1a1d
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-10-30 18:21:07 +0000

    Copy crosschain resolver to crosschain reverse resolver

---
+++ crosschain-reverse-resolver/contracts/L1Resolver.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {EVMFetcher} from '@ensdomains/evm-verifier/contracts/EVMFetcher.sol';
+import {EVMFetchTarget} from '@ensdomains/evm-verifier/contracts/EVMFetchTarget.sol';
+import {IEVMVerifier} from '@ensdomains/evm-verifier/contracts/IEVMVerifier.sol';
+
+contract L1Resolver is EVMFetchTarget {
+    using EVMFetcher for EVMFetcher.EVMFetchRequest;
+    IEVMVerifier immutable verifier;
+    address immutable target;
+    uint256 constant COIN_TYPE_ETH = 60;
+    uint256 constant RECORD_VERSIONS_SLOT = 1;
+    uint256 constant VERSINABLE_ADDRESSES_SLOT = 3;
+    uint256 constant VERSINABLE_HASHES_SLOT = 4;
+    uint256 constant VERSINABLE_TEXTS_SLOT = 11;
+
+    constructor(IEVMVerifier _verifier, address _target) {
+        verifier = _verifier;
+        target = _target;
+    }
+
+    /**
+     * Returns the address associated with an ENS node.
+     * @param node The ENS node to query.
+     * @return The associated address.
+     */
+    function addr(bytes32 node) public view returns (address) {
+        EVMFetcher.newFetchRequest(verifier, target)
+            .getStatic(RECORD_VERSIONS_SLOT)
+              .element(node)
+            .getDynamic(VERSINABLE_ADDRESSES_SLOT)
+              .ref(0)
+              .element(node)
+              .element(COIN_TYPE_ETH)
+            .fetch(this.addrCallback.selector, ''); // recordVersions
+    }
+
+    function addrCallback(
+        bytes[] memory values,
+        bytes memory
+    ) public pure returns (address) {
+        return address(bytes20(values[1]));
+    }
+
+    /**
+     * Returns the address associated with an ENS node.
+     * @param node The ENS node to query.
+     * @param coinType The cointype to query
+     * @return The associated address.
+     */
+    function addr(
+        bytes32 node,
+        uint256 coinType
+    ) public view returns (bytes memory) {
+        EVMFetcher.newFetchRequest(verifier, target)
+            .getStatic(RECORD_VERSIONS_SLOT)
+              .element(node)
+            .getDynamic(VERSINABLE_ADDRESSES_SLOT)
+              .ref(0)
+              .element(node)
+              .element(coinType)
+            .fetch(this.addrCoinTypeCallback.selector, '');
+    }
+
+    function addrCoinTypeCallback(
+        bytes[] memory values,
+        bytes memory
+    ) public pure returns (bytes memory) {
+        return values[1];
+    }
+
+    /**
+     * Returns the text data associated with an ENS node and key.
+     * @param node The ENS node to query.
+     * @param key The text data key to query.
+     * @return The associated text data.
+     */
+    function text(
+        bytes32 node,
+        string calldata key
+    ) public view returns (string memory) {
+        EVMFetcher.newFetchRequest(verifier, target)
+            .getStatic(RECORD_VERSIONS_SLOT)
+              .element(node)
+            .getDynamic(VERSINABLE_TEXTS_SLOT)
+              .ref(0)
+              .element(node)
+              .element(key)
+            .fetch(this.textCallback.selector, '');
+    }
+
+    function textCallback(
+        bytes[] memory values,
+        bytes memory
+    ) public pure returns (string memory) {
+        return string(values[1]);
+    }
+
+    /**
+     * Returns the contenthash associated with an ENS node.
+     * @param node The ENS node to query.
+     * @return The associated contenthash.
+     */
+    function contenthash(bytes32 node) public view returns (bytes memory) {
+        EVMFetcher.newFetchRequest(verifier, target)
+            .getStatic(RECORD_VERSIONS_SLOT)
+              .element(node)
+            .getDynamic(VERSINABLE_HASHES_SLOT)
+              .ref(0)
+              .element(node)
+            .fetch(this.contenthashCallback.selector, '');
+    }
+
+    function contenthashCallback(
+        bytes[] memory values,
+        bytes memory
+    ) public pure returns (bytes memory) {
+        return values[1];
+    }
+
+}
---
+++ crosschain-reverse-resolver/test/testResolver.ts
@@ -0,0 +1,146 @@
+import { makeL1Gateway } from '@ensdomains/l1-gateway';
+import { Server } from '@chainlink/ccip-read-server';
+import { HardhatEthersProvider } from '@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider';
+import type { HardhatEthersHelpers } from '@nomicfoundation/hardhat-ethers/types';
+import { expect } from 'chai';
+import {
+  BrowserProvider,
+  Contract,
+  JsonRpcProvider,
+  Signer,
+  ethers as ethersT
+} from 'ethers';
+import { FetchRequest } from 'ethers';
+import { ethers } from 'hardhat';
+import { EthereumProvider } from 'hardhat/types';
+import request from 'supertest';
+const node = '0x80ee077a908dffcf32972ba13c2df16b42688e1de21bcf17d3469a8507895eae'
+
+type ethersObj = typeof ethersT &
+  Omit<HardhatEthersHelpers, 'provider'> & {
+    provider: Omit<HardhatEthersProvider, '_hardhatProvider'> & {
+      _hardhatProvider: EthereumProvider;
+    };
+  };
+
+declare module 'hardhat/types/runtime' {
+  const ethers: ethersObj;
+  interface HardhatRuntimeEnvironment {
+    ethers: ethersObj;
+  }
+}
+
+describe('Crosschain Resolver', () => {
+  let provider: BrowserProvider;
+  let signer: Signer;
+  let verifier: Contract;
+  let target: Contract;
+  let l2contract: Contract;
+
+  before(async () => {
+    // Hack to get a 'real' ethers provider from hardhat. The default `HardhatProvider`
+    // doesn't support CCIP-read.
+    provider = new ethers.BrowserProvider(ethers.provider._hardhatProvider);
+    // provider.on("debug", (x: any) => console.log(JSON.stringify(x, undefined, 2)));
+    signer = await provider.getSigner(0);
+    const gateway = makeL1Gateway(provider as unknown as JsonRpcProvider);
+    const server = new Server()
+    gateway.add(server)
+    const app = server.makeApp('/')
+    const getUrl = FetchRequest.createGetUrlFunc();    
+    ethers.FetchRequest.registerGetUrl(async (req: FetchRequest) => {
+      if(req.url != "test:") return getUrl(req);
+
+      const r = request(app).post('/');
+      if (req.hasBody()) {
+        r.set('Content-Type', 'application/json').send(
+          ethers.toUtf8String(req.body)
+        );
+      }
+      const response = await r;
+      return {
+        statusCode: response.statusCode,
+        statusMessage: response.ok ? 'OK' : response.statusCode.toString(),
+        body: ethers.toUtf8Bytes(JSON.stringify(response.body)),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+    });
+    const l1VerifierFactory = await ethers.getContractFactory(
+      'L1Verifier',
+      signer
+    );
+    verifier = await l1VerifierFactory.deploy(['test:']);
+
+    const testL2Factory = await ethers.getContractFactory(
+      'OwnedResolver',
+      signer
+    );
+    l2contract = await testL2Factory.deploy();
+
+    const testL1Factory = await ethers.getContractFactory(
+      'L1Resolver',
+      signer
+    );
+    target = await testL1Factory.deploy(await verifier.getAddress(), await l2contract.getAddress());
+
+    // Mine an empty block so we have something to prove against
+    await provider.send('evm_mine', []);
+  });
+
+  it("should test empty ETH Address", async() => {
+    const addr = '0x0000000000000000000000000000000000000000'
+    await l2contract.clearRecords(node)
+    const result = await l2contract['addr(bytes32)'](node)
+    expect(ethers.getAddress(result)).to.equal(addr);
+    await provider.send("evm_mine", []);
+    const result2 = await target['addr(bytes32)'](node, { enableCcipRead: true })
+    expect(result2).to.equal(addr);
+  })
+
+  it("should test ETH Address", async() => {
+    const addr = '0x5A384227B65FA093DEC03Ec34e111Db80A040615'
+    await l2contract.clearRecords(node)
+    await l2contract['setAddr(bytes32,address)'](node, addr)
+    const result = await l2contract['addr(bytes32)'](node)
+    expect(ethers.getAddress(result)).to.equal(addr);
+    await provider.send("evm_mine", []);
+    const result2 = await target['addr(bytes32)'](node, { enableCcipRead: true })
+    expect(result2).to.equal(addr);
+  })
+  it("should test non ETH Address", async() => {
+    const addr = '0x76a91462e907b15cbf27d5425399ebf6f0fb50ebb88f1888ac'
+    const coinType = 0 // BTC
+    await l2contract.clearRecords(node)
+    await l2contract['setAddr(bytes32,uint256,bytes)'](node, coinType, addr)
+    const result = await l2contract['addr(bytes32,uint256)'](node, 0)
+    expect(result).to.equal(addr);
+    await provider.send("evm_mine", []);
+    const result2 = await target['addr(bytes32,uint256)'](node, coinType, { enableCcipRead: true })
+    expect(result2).to.equal(addr);
+  })
+
+  it("should test text record", async() => {
+    const key = 'name'
+    const value = 'nick.eth'
+    await l2contract.clearRecords(node)
+    await l2contract.setText(node, key, value)
+    await provider.send("evm_mine", []);
+    const result = await l2contract.text(node, key)
+    expect(result).to.equal(value);
+    const result2 = await target.text(node, key, { enableCcipRead: true })
+    expect(result2).to.equal(value);
+  })
+
+  it("should test contenthash", async() => {
+    const contenthash = '0xe3010170122029f2d17be6139079dc48696d1f582a8530eb9805b561eda517e22a892c7e3f1f'
+    await l2contract.clearRecords(node)
+    await l2contract.setContenthash(node, contenthash)
+    await provider.send("evm_mine", []);
+    const result = await l2contract.contenthash(node)
+    expect(result).to.equal(contenthash);
+    const result2 = await target.contenthash(node, { enableCcipRead: true })
+    expect(result2).to.equal(contenthash);
+  })
+});
