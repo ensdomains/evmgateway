b6351524d0461336255673a77025272501dfb62e
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-11-06 15:18:36 +0000

    Update storage slot info

--- crosschain-reverse-resolver/contracts/deps.sol
+++ crosschain-reverse-resolver/contracts/deps.sol
@@ -3,18 +3,11 @@ import '@ensdomains/ens-contracts/contracts/resolvers/OwnedResolver.sol';
 import '@ensdomains/ens-contracts/contracts/reverseRegistrar/L2ReverseRegistrar.sol';
 
 // Storage slot
-// ┌─────────────────────┬──────────────────────────────┬──────────────┬
-// │      contract       │        state_variable        │ storage_slot │
-// ├─────────────────────┼──────────────────────────────┼──────────────┼
-// |    OwnedResolver    │            _owner            │      0       │
-// │    OwnedResolver    │        recordVersions        │      1       │
-// │    OwnedResolver    │       versionable_abis       │      2       │
-// │    OwnedResolver    │    versionable_addresses     │      3       │
-// │    OwnedResolver    │      versionable_hashes      │      4       │
-// │    OwnedResolver    │    versionable_zonehashes    │      5       │
-// │    OwnedResolver    │     versionable_records      │      6       │
-// │    OwnedResolver    │ versionable_nameEntriesCount │      7       │
-// │    OwnedResolver    │    versionable_interfaces    │      8       │
-// │    OwnedResolver    │      versionable_names       │      9       │
-// │    OwnedResolver    │     versionable_pubkeys      │      10      │
-// │    OwnedResolver    │      versionable_texts       │      11      │
+// ┌───────────────────────┬──────────────────────────────┬──────────────┬
+// │      contract         │        state_variable        │ storage_slot │
+// ├───────────────────────┼──────────────────────────────┼──────────────┼
+// |  L2ReverseRegistrar   │            _owner            │      0       │
+// │  L2ReverseRegistrar   │        recordVersions        │      1       │
+// │  L2ReverseRegistrar   │         lastUpdated          │      2       │
+// │  L2ReverseRegistrar   │      versionable_texts       │      3       │
+// │  L2ReverseRegistrar   │      versionable_names       │      4       │
