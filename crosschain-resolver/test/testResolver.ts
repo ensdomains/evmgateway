a5665fd9051869c3a34a8924bfdeae93abb852e8
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-11-08 21:04:23 +0000

    Remove console.log

--- crosschain-resolver/test/testResolver.ts
+++ crosschain-resolver/test/testResolver.ts
@@ -44,7 +44,7 @@ describe('Crosschain Resolver', () => {
   let ens: Contract;
   let wrapper: Contract;
   let baseRegistrar: Contract;
-  let signerAddress, resolverAddress, wrapperAddress, metaDataserviceAddress
+  let signerAddress, resolverAddress, wrapperAddress
 
   before(async () => {
     // Hack to get a 'real' ethers provider from hardhat. The default `HardhatProvider`
@@ -81,30 +81,19 @@ describe('Crosschain Resolver', () => {
     ens = await ensFactory.deploy();
     const ensAddress = await ens.getAddress()
     const baseRegistrarFactory = await ethers.getContractFactory('BaseRegistrarImplementation',signer);
-    console.log({ensAddress, namehash:ethers.namehash('eth')})
     baseRegistrar = await baseRegistrarFactory.deploy(ensAddress,ethers.namehash('eth'))
     const baseRegistrarAddress = await baseRegistrar.getAddress()
-    console.log(111, {baseRegistrarAddress, signerAddress})
     await baseRegistrar.addController(signerAddress)
-    console.log(112)
-    console.log(113)
     const metaDataserviceFactory = await ethers.getContractFactory('StaticMetadataService',signer);
     const metaDataservice = await metaDataserviceFactory.deploy('https://ens.domains')
     const metaDataserviceAddress = await metaDataservice.getAddress()
-    console.log({metaDataserviceAddress})
     const reverseRegistrarFactory = await ethers.getContractFactory('ReverseRegistrar',signer);
     const reverseRegistrar = await reverseRegistrarFactory.deploy(ensAddress)
     const reverseRegistrarAddress = await reverseRegistrar.getAddress()
-    console.log({reverseRegistrarAddress})
     await ens.setSubnodeOwner(EMPTY_BYTES32, labelhash('reverse'), signerAddress)
-    console.log(114)
     await ens.setSubnodeOwner(ethers.namehash('reverse'),labelhash('addr'), reverseRegistrarAddress)
-    console.log(115)
     await ens.setSubnodeOwner(EMPTY_BYTES32, labelhash('eth'), baseRegistrarAddress)
-    console.log(116)
     await baseRegistrar.register(labelhash('foo'), signerAddress, 100000000)
-    // await ens.setSubnodeOwner(ethers.namehash('eth'), labelhash('foo'), signerAddress)
-    console.log(11789, await ens.owner(node), signerAddress)
     const publicResolverFactory = await ethers.getContractFactory('PublicResolver',signer);
     const publicResolver = await publicResolverFactory.deploy(
       ensAddress,
@@ -113,64 +102,46 @@ describe('Crosschain Resolver', () => {
       reverseRegistrarAddress,
     )
     const publicResolverAddress = await publicResolver.getAddress()
-    console.log({publicResolverAddress})
     await reverseRegistrar.setDefaultResolver(publicResolverAddress)
 
-    console.log(4, {ensAddress,baseRegistrarAddress, metaDataserviceAddress})
     const wrapperFactory = await ethers.getContractFactory('NameWrapper',signer);
-    console.log(5)
     await provider.send('evm_mine', []);
-    console.log(6)
     wrapper = await wrapperFactory.deploy(
       ensAddress,
       baseRegistrarAddress,
       metaDataserviceAddress
     );
-    console.log(7, {wrapperAddress})
     wrapperAddress = await wrapper.getAddress()
-    console.log(8)
     const l1VerifierFactory = await ethers.getContractFactory(
       'L1Verifier',
       signer
     );
     verifier = await l1VerifierFactory.deploy(['test:']);
-    console.log(8)
     const impl = await ethers.getContractFactory(
       'DelegatableResolver',
       signer
     );
-    console.log(9)
     const implContract = await impl.deploy();
-    console.log(10)
     const testL2Factory = await ethers.getContractFactory(
       'DelegatableResolverFactory',
       signer
     );
-    console.log(11)
     const l2factoryContract = await testL2Factory.deploy(await implContract.getAddress());
-    console.log(12)
     const tx = await l2factoryContract.create(await signer.getAddress());
     await provider.send('evm_mine', []);
-    console.log(13)
     await tx.wait()
     const logs = await l2factoryContract.queryFilter("NewDelegatableResolver")
     const [resolver] = logs[0].args
     resolverAddress = resolver
-    console.log(14)
     const testL1Factory = await ethers.getContractFactory(
       'L1Resolver',
       signer
     );
-    console.log(15)
     const verifierAddress = await verifier.getAddress()
-    console.log(16, {verifierAddress, ensAddress, wrapperAddress})
     target = await testL1Factory.deploy(verifierAddress, ensAddress, wrapperAddress);
-    console.log(17)
     // Mine an empty block so we have something to prove against
     await provider.send('evm_mine', []);
-    console.log(18)
     l2contract = impl.attach(resolverAddress)
-    console.log(19)
   });
 
   it("should not allow non owner to set target", async() => {
@@ -191,28 +162,20 @@ describe('Crosschain Resolver', () => {
   })
 
   it("should allow wrapped owner to set target", async() => {
-    console.log(20)
     const label = 'wrapped'
     const tokenId = labelhash(label)
-    console.log(21, {tokenId, label, signerAddress})
     await baseRegistrar.setApprovalForAll(wrapperAddress, true)
-    console.log(22, {wrapperAddress})    
     await baseRegistrar.register(tokenId, signerAddress, 100000000)
-    console.log(23)
     await wrapper.wrapETH2LD(
       label,
       signerAddress,
       0, // CAN_DO_EVERYTHING
       EMPTY_ADDRESS,
     )
-    console.log(24)
     const wrappedtnode = ethers.namehash(`${label}.eth`)
-    console.log(25, {wrappedtnode, resolverAddress})
     await target.setTarget(wrappedtnode, resolverAddress)
-    console.log(26)
     expect(await target.targets(wrappedtnode)).to.equal(resolverAddress);
   })
-
 
   it("should test empty ETH Address", async() => {
     await target.setTarget(node, resolverAddress)
@@ -234,7 +197,6 @@ describe('Crosschain Resolver', () => {
     expect(ethers.getAddress(result)).to.equal(addr);
     await provider.send("evm_mine", []);
     const result2 = await target['addr(bytes32)'](node, { enableCcipRead: true })
-    console.log(111,{result, result2, addr})
     expect(result2).to.equal(addr);
   })
   it("should test non ETH Address", async() => {
@@ -247,7 +209,6 @@ describe('Crosschain Resolver', () => {
     expect(result).to.equal(addr);
     await provider.send("evm_mine", []);
     const result2 = await target['addr(bytes32,uint256)'](node, coinType, { enableCcipRead: true })
-    console.log(112,{result, result2, addr})
     expect(result2).to.equal(addr);
   })
 
