540275151b822a9359a028ccdf865c3b73ace7d7
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-12-05 18:11:05 +0000

    Update comment, link to ENSIP16 and add variable name on the return argument of metadata function

--- crosschain-resolver/contracts/IMetadataResolver.sol
+++ crosschain-resolver/contracts/IMetadataResolver.sol
@@ -3,16 +3,23 @@ pragma solidity ^0.8.17;
 
 interface IMetadataResolver {
     /**
-     * @notice Get metadata about the CCIP Resolver
+     * @notice Get metadata about the CCIP Resolver ENSIP 16 https://docs.ens.domains/ens-improvement-proposals/ensip-16-offchain-metadata
      * @dev This function provides metadata about the CCIP Resolver, including its name, coin type, GraphQL URL, storage type, and encoded information.
      * @param name The domain name in format (dnsEncoded)
-     * @return name The name of the resolver ("CCIP RESOLVER")
+     * @return resolverName The name of the resolver ("CCIP RESOLVER")
      * @return coinType Resolvers coin type (60 for Ethereum)
      * @return graphqlUrl The GraphQL URL used by the resolver
-     * @return storageType Storage Type (0 for EVM)
+     * @return storageType 0 = EVM, 1 = Non blockchain, 2 = Starknet
      * @return storageLocation The storage identifier. For EVM chains, this is the address of the resolver contract.
      * @return context can be l2 resolver contract address for evm chain but can be any l2 storage identifier for non evm chain
      *
      */
-    function metadata(bytes calldata name) external view returns (string memory, uint256, string memory, uint8, bytes memory, bytes memory);
+    function metadata(bytes calldata name) external view returns (
+        string memory resolverName,
+        uint256 coinType,
+        string memory graphqlUrl,
+        uint8 storageType,
+        bytes memory storageLocation,
+        bytes memory context
+    );
 }
