408e569a5e327a00e29c6c7336de0a830db5bd1d
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-12-19 16:28:07 +0900

    Add Tracker

--- arb-gateway/src/worker.ts
+++ arb-gateway/src/worker.ts
@@ -1,14 +1,34 @@
+import { Request as CFWRequest } from '@cloudflare/workers-types';
 import { Server } from '@ensdomains/ccip-read-cf-worker';
 import type { Router } from '@ensdomains/evm-gateway';
 import { InMemoryBlockCache } from './blockCache/InMemoryBlockCache.js';
+import { Tracker } from './tracker';
 interface Env {
   L1_PROVIDER_URL: string;
   L2_PROVIDER_URL: string;
   L2_ROLLUP: string;
 }
 
+const tracker = new Tracker('arb-gateway-worker.ens-cf.workers.dev', {enableLogging:true});
+const logResult = async (request:CFWRequest, result: Response) => {
+  console.log({request, result})
+  if (!result.body) {
+    return result;
+  }
+  const [streamForLog, streamForResult] = result.body.tee();
+  const logResult = await new Response(streamForLog).json();
+
+  await tracker.trackEvent(
+    'result',
+    request,
+    { props: { result: logResult.data.substring(0, 200) } },
+    true
+  );
+  return new Response(streamForResult, result);
+};
+
 let app: Router;
-async function fetch(request: Request, env: Env) {
+async function fetchGateway(request: CFWRequest, env: Env) {
   // Loading libraries dynamically as a temp work around.
   // Otherwise, deployment thorws "Error: Script startup exceeded CPU time limit." error
   if (!app) {
@@ -37,9 +57,9 @@ async function fetch(request: Request, env: Env) {
     gateway.add(server);
     app = server.makeApp('/');
   }
-  return app.handle(request);
+  return app.handle(request).then(logResult.bind(this, request));
 }
 
 export default {
-  fetch,
+  fetch:fetchGateway,
 };
