2e67fee0d63ed88de7857cab8ba9728e00fc3821
AlexNi245 <alex.plutta@googlemail.com>
2024-03-11 14:48:02 +0100

    add comments

--- dm3-names/contracts/Dm3NameRegistrar.sol
+++ dm3-names/contracts/Dm3NameRegistrar.sol
@@ -3,29 +3,55 @@ pragma solidity 0.8.17;
 
 import {IAddrResolver} from '@ensdomains/ens-contracts/contracts/resolvers/profiles/IAddrResolver.sol';
 import {INameResolver} from '@ensdomains/ens-contracts/contracts/resolvers/profiles/INameResolver.sol';
+import {ITextResolver, TextChanged} from '@ensdomains/ens-contracts/contracts/resolvers/profiles/ITextResolver.sol';
 
-contract Dm3NameRegistrar is IAddrResolver, INameResolver {
+/// @title Dm3NameRegistrar
+/// @notice This contract is used for registering names in the ENS system. It is a combination of ENSResolver and ReverseRegistrar contracts. Allowing to register names and set text records for each name. By beeing compatible with ENSResolver and ReverseRegistrar
+
+contract Dm3NameRegistrar is IAddrResolver, INameResolver, ITextResolver {
+    //Lookup table for hexadecimal conversion
+    //Taken from ENS ReverseRegistrar contract
+    //https://github.com/ensdomains/ens-contracts/blob/21736916300b26cb8ea1802dbf6c9ff054adaeab/contracts/reverseRegistrar/ReverseRegistrar.sol#L12
     bytes32 private constant lookup =
         0x3031323334353637383961626364656600000000000000000000000000000000;
 
+    // Constant for reverse node address
+    //Taken from ENS ReverseRegistrar contract
+    //https://github.com/ensdomains/ens-contracts/blob/21736916300b26cb8ea1802dbf6c9ff054adaeab/contracts/reverseRegistrar/ReverseRegistrar.sol#L12
     bytes32 private constant ADDR_REVERSE_NODE =
         0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
+
+    //Node of the L1 domain. in case of OP name that would be namehash(op.dm3.eth)
+    //Is not immutable so it can be retrieved from the storage using CCIP
     bytes32 public PARENT_NODE;
 
+    // Mapping to store the owner of each node
     mapping(bytes32 => address) public owner;
+
+    // Mapping to store the reverse record of each node
     mapping(bytes32 => string) public reverse;
 
+    // Mapping to store text records for each node
+    mapping(bytes32 => mapping(string => string)) public texts;
+
+    // Event emitted when a name is registered
     event NameRegistered(address indexed addr, string indexed name);
+
+    // Event emitted when a name is removed
     event NameRemoved(address indexed addr, string indexed name);
 
+    /// @notice Constructor to set the parent node
+    /// @param _parentNode The parent node of the ENS
     constructor(bytes32 _parentNode) {
         PARENT_NODE = _parentNode;
     }
 
+    /// @notice Register a name in the ENS system
+    /// @param _name The name to register
     function register(string calldata _name) external {
         string memory oldName = reverse[makeReverseNode(msg.sender)];
         if (bytes(_name).length == 0) {
-            //Clear name
+            // Clear name if the new name is empty
             delete owner[makeLabelNode(oldName)];
             delete reverse[makeReverseNode(msg.sender)];
             emit NameRemoved(msg.sender, oldName);
@@ -33,29 +59,64 @@ contract Dm3NameRegistrar is IAddrResolver, INameResolver {
         }
 
         if (bytes(oldName).length > 0) {
-            //Clear name
+            // Clear old name if it exists
             delete owner[makeLabelNode(oldName)];
             emit NameRemoved(msg.sender, oldName);
         }
-
+        //set owner record
         owner[makeLabelNode(_name)] = msg.sender;
-
         //set reverse record
         reverse[makeReverseNode(msg.sender)] = _name;
-
+        //emit NameRegistered event
         emit NameRegistered(msg.sender, _name);
     }
-
+    /// @notice Set text for a label
+    /// @param label The label to set the text for
+    /// @param key The key for the text
+    /// @param value The text to set
+    function setText(
+        bytes32 label,
+        string calldata key,
+        string calldata value
+    ) external {
+        address owner = owner[label];
+        require(owner != address(0), 'Name not registered');
+        require(owner == msg.sender, 'Only owner');
+        texts[label][key] = value;
+        emit TextChanged(node, key, key, value);
+    }
+    /// @notice Get the address of a node
+    /// @param node The node to get the address for
+    /// @return The address of the node
     function addr(bytes32 node) external view returns (address payable) {
         return payable(owner[node]);
     }
+
+    /// @notice Get the name of a node
+    /// @param node The node to get the name for
+    /// @return The name of the node
     function name(bytes32 node) external view returns (string memory) {
         return reverse[node];
     }
+
+    /// @notice Get the text of a node
+    /// @param node The node to get the text for
+    /// @param key The key for the text
+    /// @return The text of the node
+    function text(
+        bytes32 node,
+        string calldata key
+    ) external view override returns (string memory) {
+        return texts[node][key];
+    }
+    /// @notice Make a label node using the PARENT_NODE
+    /// @param label The label to make a node for
+    /// @return The node of the label
     function makeLabelNode(string memory label) private view returns (bytes32) {
         return
             keccak256(abi.encodePacked(PARENT_NODE, keccak256(bytes(label))));
     }
+    /// @notice Make a label node used for the reverse record using the ADDR_REVERSE_NODE
     function makeReverseNode(address addr) private pure returns (bytes32) {
         return
             keccak256(
@@ -63,6 +124,11 @@ contract Dm3NameRegistrar is IAddrResolver, INameResolver {
             );
     }
 
+    /// @notice Convert an address to a hexadecimal string and hash it
+    /// @param addr The address to convert and hash
+    /// @dev taken from ENS ReverseRegistrar contract
+    /// @dev https://github.com/ensdomains/ens-contracts/blob/21736916300b26cb8ea1802dbf6c9ff054adaeab/contracts/reverseRegistrar/ReverseRegistrar.sol#L164
+    /// @return The hashed hexadecimal string of the address
     function sha3HexAddress(address addr) private pure returns (bytes32 ret) {
         assembly {
             for {
--- dm3-names/test/testDm3NameRegistrar.ts
+++ dm3-names/test/testDm3NameRegistrar.ts
@@ -23,7 +23,7 @@ describe.only('Dm3 name registrar', () => {
   let target: Contract;
   let signer: ethers.Signer;
 
-  before(async () => {
+  beforeEach(async () => {
     const Dm3NameRegistrarFactory =
       await ethers.getContractFactory('Dm3NameRegistrar');
     const parentNode = ethers.namehash('op.dm3.eth');
@@ -31,76 +31,105 @@ describe.only('Dm3 name registrar', () => {
     signer = (await ethers.getSigners())[0];
   });
 
-  it('can set dm3 name', async () => {
-    await target.register('alice');
-    const reverseRecord = `${signer.address
-      .slice(2)
-      .toLowerCase()}.addr.reverse`;
+  describe('register', () => {
+    it('can set dm3 name', async () => {
+      await target.register('alice');
+      const reverseRecord = `${signer.address
+        .slice(2)
+        .toLowerCase()}.addr.reverse`;
 
-    const owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
-    const name = await target.reverse(ethers.namehash(reverseRecord));
+      const owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
+      const name = await target.reverse(ethers.namehash(reverseRecord));
 
-    expect(owner).to.equal(signer.address);
-    expect(name).to.equal('alice');
+      expect(owner).to.equal(signer.address);
+      expect(name).to.equal('alice');
+    });
+    it('can use addr to retrive address of node', async () => {
+      await target.register('alice');
+
+      const addr = await target.addr(ethers.namehash('alice.op.dm3.eth'));
+      expect(addr).to.equal(signer.address);
+    });
+    it('can use reverse record to retrive name of address', async () => {
+      await target.register('alice');
+      const reverseRecord = `${signer.address
+        .slice(2)
+        .toLowerCase()}.addr.reverse`;
+      const reverseNode = ethers.namehash(reverseRecord);
+
+      const name = await target.name(reverseNode);
+      expect(name).to.equal('alice');
+    });
+    it('registering a new name overrides the old name', async () => {
+      await target.register('alice');
+      const reverseRecord = `${signer.address
+        .slice(2)
+        .toLowerCase()}.addr.reverse`;
+
+      let owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
+      let name = await target.reverse(ethers.namehash(reverseRecord));
+
+      expect(owner).to.equal(signer.address);
+      expect(name).to.equal('alice');
+
+      await target.register('bob');
+
+      owner = await target.owner(ethers.namehash('bob.op.dm3.eth'));
+      name = await target.reverse(ethers.namehash(reverseRecord));
+
+      const oldOwner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
+
+      expect(owner).to.equal(signer.address);
+      expect(name).to.equal('bob');
+
+      expect(oldOwner).to.equal(ethers.ZeroAddress);
+    });
+    it('passing an empty name deletes an existing record', async () => {
+      await target.register('alice');
+      const reverseRecord = `${signer.address
+        .slice(2)
+        .toLowerCase()}.addr.reverse`;
+
+      let owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
+      let name = await target.reverse(ethers.namehash(reverseRecord));
+
+      expect(owner).to.equal(signer.address);
+      expect(name).to.equal('alice');
+
+      await target.register(ethers.toUtf8Bytes(''));
+
+      owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
+      name = await target.reverse(ethers.namehash(reverseRecord));
+
+      expect(owner).to.equal(ethers.ZeroAddress);
+      expect(name).to.equal('');
+    });
   });
-  it('can use addr to retrive address of node', async () => {
-    await target.register('alice');
 
-    const addr = await target.addr(ethers.namehash('alice.op.dm3.eth'));
-    expect(addr).to.equal(signer.address);
-  });
-  it('can use reverse record to retrive name of address', async () => {
-    await target.register('alice');
-    const reverseRecord = `${signer.address
-      .slice(2)
-      .toLowerCase()}.addr.reverse`;
-    const reverseNode = ethers.namehash(reverseRecord);
+  describe('setText', () => {
+    it('can set text record if name has been registered before', async () => {
+      await target.register('alice');
+      await target.setText(ethers.namehash('alice.op.dm3.eth'), 'key', 'value');
+      const value = await target.text(
+        ethers.namehash('alice.op.dm3.eth'),
+        'key'
+      );
+      expect(value).to.equal('value');
+    });
 
-    const name = await target.name(reverseNode);
-    expect(name).to.equal('alice');
-  });
-  it('registering a new name overrides the old name', async () => {
-    await target.register('alice');
-    const reverseRecord = `${signer.address
-      .slice(2)
-      .toLowerCase()}.addr.reverse`;
-
-    let owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
-    let name = await target.reverse(ethers.namehash(reverseRecord));
-
-    expect(owner).to.equal(signer.address);
-    expect(name).to.equal('alice');
-
-    await target.register('bob');
-
-    owner = await target.owner(ethers.namehash('bob.op.dm3.eth'));
-    name = await target.reverse(ethers.namehash(reverseRecord));
-
-    const oldOwner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
-
-    expect(owner).to.equal(signer.address);
-    expect(name).to.equal('bob');
-
-    expect(oldOwner).to.equal(ethers.ZeroAddress);
-  });
-  it('passing an empty name deletes an existing record', async () => {
-    await target.register('alice');
-    const reverseRecord = `${signer.address
-      .slice(2)
-      .toLowerCase()}.addr.reverse`;
-
-    let owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
-    let name = await target.reverse(ethers.namehash(reverseRecord));
-
-    expect(owner).to.equal(signer.address);
-    expect(name).to.equal('alice');
-
-    await target.register(ethers.toUtf8Bytes(''));
-
-    owner = await target.owner(ethers.namehash('alice.op.dm3.eth'));
-    name = await target.reverse(ethers.namehash(reverseRecord));
-
-    expect(owner).to.equal(ethers.ZeroAddress);
-    expect(name).to.equal('');
+    it('reverts if name has not been registered', async () => {
+      await expect(
+        target.setText(ethers.namehash('alice.op.dm3.eth'), 'key', 'value')
+      ).to.be.revertedWith('Name not registered');
+    });
+    it('reverts if msg.sender is not owner of name', async () => {
+      await target.register('alice');
+      const other = (await ethers.getSigners())[1];
+      await expect(
+        target
+          .connect(other)
+          .setText(ethers.namehash('alice.op.dm3.eth'), 'key', 'value')
+      ).to.be.revertedWith('Only owner');
+    });
   });
 });
