61c19c76c8de1db3988abcdb6f291a2d13e3fd1f
Chomtana <Chomtana001@gmail.com>
2024-03-26 11:04:22 +0700

    try upgrading dependencies

--- op-verifier/hardhat.config.ts
+++ op-verifier/hardhat.config.ts
@@ -5,8 +5,8 @@ import { HardhatUserConfig } from 'hardhat/config';
 import 'ethers'
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 const L1_PROVIDER_URL = process.env.L1_PROVIDER_URL || '';
-const L1_ETHERSCAN_API_KEY = process.env.L1_ETHERSCAN_API_KEY;
-const L2_ETHERSCAN_API_KEY = process.env.L2_ETHERSCAN_API_KEY;
+const L1_ETHERSCAN_API_KEY = process.env.L1_ETHERSCAN_API_KEY || '';
+const L2_ETHERSCAN_API_KEY = process.env.L2_ETHERSCAN_API_KEY || '';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
