f12c91eca4309b95f11383180b31668c59802adf
Chomtana <Chomtana001@gmail.com>
2024-04-04 01:50:05 +0700

    deploy cloudflare worker

--- op-gateway/src/server.ts
+++ op-gateway/src/server.ts
@@ -2,8 +2,11 @@ import { Server } from '@chainlink/ccip-read-server';
 import { Command } from '@commander-js/extra-typings';
 import { EVMGateway } from '@ensdomains/evm-gateway';
 import { JsonRpcProvider } from 'ethers';
-import { OPProofService } from './OPProofService.js';
-import { OPDisputeGameProofService } from './OPDisputeGameProofService.js';
+import { OPProofService, type OPProvableBlock } from './OPProofService.js';
+import {
+  OPDisputeGameProofService,
+  type OPProvableBlock as OPDisputeGameProvableBlock,
+} from './OPDisputeGameProofService.js';
 import type { IProofService } from '@ensdomains/evm-gateway';
 
 const program = new Command()
@@ -23,7 +26,11 @@ const program = new Command()
     'address for L2 output oracle on the L1',
     ''
   )
-  .option('-d, --delay <number>', 'number of delay to use (in minutes for DisputeGame)', '5')
+  .option(
+    '-d, --delay <number>',
+    'number of delay to use (in minutes for DisputeGame)',
+    '5'
+  )
   .option('-t, --type <number>', '0 - L2OutputOracle, 1 - DisputeGame', '1');
 
 program.parse();
@@ -34,22 +41,22 @@ program.parse();
   const l1Provider = new JsonRpcProvider(options.l1ProviderUrl);
   const l2Provider = new JsonRpcProvider(options.l2ProviderUrl);
 
-  let proofService: IProofService<any>
-  
+  let proofService: IProofService<OPProvableBlock | OPDisputeGameProvableBlock>;
+
   if (Number(options.type) == 0) {
     proofService = new OPProofService(
       l1Provider,
       l2Provider,
       options.l2OutputOracle,
       Number(options.delay)
-    )
+    );
   } else if (Number(options.type) == 1) {
     proofService = new OPDisputeGameProofService(
       l1Provider,
       l2Provider,
       options.l2OutputOracle,
       Number(options.delay)
-    )
+    );
   } else {
     throw new Error('Invalid output oracle type');
   }
--- op-gateway/src/worker.ts
+++ op-gateway/src/worker.ts
@@ -1,10 +1,13 @@
 import { Server } from '@ensdomains/ccip-read-cf-worker';
-import type { Router } from '@ensdomains/evm-gateway';
+import type { IProofService, Router } from '@ensdomains/evm-gateway';
+import type { OPProvableBlock as OPDisputeGameProvableBlock } from './OPDisputeGameProofService.js';
+import type { OPProvableBlock } from './OPProofService.js';
 interface Env {
   L1_PROVIDER_URL: string;
   L2_PROVIDER_URL: string;
   L2_OUTPUT_ORACLE: string;
   DELAY: number;
+  TYPE: number;
 }
 let app: Router;
 async function fetch(request: Request, env: Env) {
@@ -14,20 +17,35 @@ async function fetch(request: Request, env: Env) {
     const ethers = await import('ethers');
     const EVMGateway = (await import('@ensdomains/evm-gateway')).EVMGateway;
     const OPProofService = (await import('./OPProofService.js')).OPProofService;
+    const OPDisputeGameProofService = (await import('./OPDisputeGameProofService.js')).OPDisputeGameProofService;
     // Set PROVIDER_URL under .dev.vars locally. Set the key as secret remotely with `wrangler secret put WORKER_PROVIDER_URL`
-    const { L1_PROVIDER_URL, L2_PROVIDER_URL, L2_OUTPUT_ORACLE, DELAY } = env;
+    const { L1_PROVIDER_URL, L2_PROVIDER_URL, L2_OUTPUT_ORACLE, DELAY, TYPE } = env;
     const l1Provider = new ethers.JsonRpcProvider(L1_PROVIDER_URL);
     const l2Provider = new ethers.JsonRpcProvider(L2_PROVIDER_URL);
 
-    console.log({ L1_PROVIDER_URL, L2_PROVIDER_URL, DELAY });
-    const gateway = new EVMGateway(
-      new OPProofService(
+    console.log({ L1_PROVIDER_URL, L2_PROVIDER_URL, DELAY, TYPE });
+
+    let proofService: IProofService<OPProvableBlock | OPDisputeGameProvableBlock>;
+
+    if (Number(TYPE) == 0) {
+      proofService = new OPProofService(
         l1Provider,
         l2Provider,
         L2_OUTPUT_ORACLE,
         Number(DELAY)
-      )
-    );
+      );
+    } else if (Number(TYPE) == 1) {
+      proofService = new OPDisputeGameProofService(
+        l1Provider,
+        l2Provider,
+        L2_OUTPUT_ORACLE,
+        Number(DELAY),
+      );
+    } else {
+      throw new Error('Invalid output oracle type');
+    }
+
+    const gateway = new EVMGateway(proofService);
 
     const server = new Server();
     gateway.add(server);
