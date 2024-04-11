c80e72c99c4d47aebfc9aa027d1927952c9d33da
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-04-11 17:20:55 +0100

    Remove crosschain-resolver and crosschain-reverse-resolver

--- crosschain-resolver/contracts/IAddrSetter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IAddrSetter{
-    function setAddr(bytes calldata name, address _addr) external view returns (bytes memory result);
-}
-
