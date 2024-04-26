fe0749edf79eff12bc1062fcf8b8e64350beba06
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-04-26 13:14:46 +0100

    move propsDecoder

--- evm-gateway/src/utils.ts
+++ evm-gateway/src/utils.ts
@@ -1,5 +1,22 @@
 import { Request as CFWRequest } from '@cloudflare/workers-types';
-
+import { type PropsDecoder } from '@ensdomains/server-analytics';
 export interface Router {
   handle: (request: CFWRequest) => Promise<Response>;
 }
+
+export const propsDecoder: PropsDecoder<CFWRequest> = (request?: CFWRequest) => {
+  if (!request || !request.url) {
+    return {};
+  }
+  const trackingData = request.url.match(
+    /\/0x[a-fA-F0-9]{40}\/0x[a-fA-F0-9]{1,}\.json/
+  );
+  if (trackingData) {
+    return {
+      sender: trackingData[0].slice(1, 42),
+      calldata: trackingData[0].slice(44).replace('.json', ''),
+    };
+  } else {
+    return {};
+  }
+};
